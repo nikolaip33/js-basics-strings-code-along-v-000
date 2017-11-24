fe4ef6510d20b5dc5dd73bf379d8d911b47da5fa
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-24 02:08:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
-const currentUser = "Tako"
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage = welcomeMessage.uppercas()
+const currentUser = "Tako";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.uppercase();
